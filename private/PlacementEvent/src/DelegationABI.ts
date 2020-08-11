2bec56661490bf7ccf5934d1463eef2a4d1c574f
Alonzo Coeus <alonzo.coeus@mail.com>
2020-08-11 20:58:01 +0100

    Explaining why finj-ydzv being revealed has minimal to no security implications

--- private/PlacementEvent/src/DelegationABI.ts
+++ private/PlacementEvent/src/DelegationABI.ts
@@ -3,6 +3,9 @@ const landABI = [{"constant": true,"inputs": [{"name": "_interfaceID","type": "b
 
 const delegAddr = '0x17bdbacd23d223e1beecb95705d0db92b4a2a0da';
 const landAddr = '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d';
-const password = 'finj-ydzv';
+const password = 'finj-ydzv'; 
+// The above password is fine in public as the geth node only acts as a oracle key
+// To be able to use this you would have to have localhost access to the server
+// and with that you could just as much damage even if this key was hidden
 
 export {password, delegationABI, landABI, delegAddr, landAddr};
